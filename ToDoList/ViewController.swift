208c609ae75101d653ca73c196dcd4d8c46a07ca
motto3532 <gropmor74@gmail.com>
2023-06-09 23:14:57 +0900

    タスク追加機能実装

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -14,9 +14,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    var tasks: [String] = []
+    
     @IBOutlet weak var tableView: UITableView!
     
     @IBAction func addButtonAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "タスクを追加", message: "タスクを入力してください", preferredStyle: .alert)
+        
+        //追加ボタン
+        let addAction = UIAlertAction(title: "追加", style: .default) { (UIAlertAction) in
+            //テキストを取り出す
+            guard let textField = alertController.textFields?.first else {
+                return
+            }
+            guard let text = textField.text else {
+                return
+            }
+            //タスクを追加
+            self.tasks.append(text)
+            //tableViewを更新
+            self.tableView.reloadData()
+        }
+        
+        //キャンセルボタン
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel)
+        
+        //アラート作成
+        alertController.addTextField()
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
     }
     
 }
